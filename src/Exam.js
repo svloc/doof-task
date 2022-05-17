f3d6402f3cd1f563427e178ba4139670df74dc00
svloc <svloc39@gmail.com>
2022-05-17 09:52:17 +0530

    updated

--- src/Exam.js
+++ src/Exam.js
@@ -7,63 +7,54 @@ export default function Exam() {
     {
       id: 1,
       questionText: "What is the capital of France?",
-      answerOptions: [
-        { answerText: "New York", isCorrect: false },
-        { answerText: "London", isCorrect: false },
-        { answerText: "Paris", isCorrect: true },
-        { answerText: "Dublin", isCorrect: false },
-      ],
+      answerOptions: [{ answerText: "New York"},{ answerText: "London"}, { answerText: "Paris"},{ answerText: "Dublin"}],
+      isCorrect:'Paris'
+     
     },
     {
       id: 2,
       questionText: "Who is CEO of Tesla?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Elon Musk", isCorrect: true },
-        { answerText: "Bill Gates", isCorrect: false },
-        { answerText: "Tony Stark", isCorrect: false },
-      ],
+      answerOptions: [ { answerText: "Jeff Bezos"}, { answerText: "Elon Musk"},{ answerText: "Bill Gates"}, { answerText: "Tony Stark"},],
+      isCorrect:'Elon Musk'
+   
     },
     {
       id: 3,
       questionText: "The iPhone was created by which company?",
-      answerOptions: [
-        { answerText: "Apple", isCorrect: true },
-        { answerText: "Intel", isCorrect: false },
-        { answerText: "Amazon", isCorrect: false },
-        { answerText: "Microsoft", isCorrect: false },
-      ],
+      answerOptions:[{ answerText: "Apple"},{ answerText: "Intel"}, { answerText: "Amazon"},{ answerText: "Microsoft"}, ],
+      isCorrect:'Apple'
     },
     {
       id: 4,
       questionText: "How many Harry Potter books are there?",
-      answerOptions: [
-        { answerText: "1", isCorrect: false },
-        { answerText: "4", isCorrect: false },
-        { answerText: "6", isCorrect: false },
-        { answerText: "7", isCorrect: true },
-      ],
+      answerOptions: [ { answerText: "1"},{ answerText: "4"}, { answerText: "6"}, { answerText: "7"}],
+      isCorrect:'7'
     },
   ];
-  const [name, setName] = useState(1);
+
+  const [ans, setAns] = useState('');
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [totalScore, setTotalScore] = useState(0);
   const [submitButton, setSubmitButton] = useState(false);
-  const inputRef = useRef();
+  const [isActive,setIsActive]=useState(false)
+  var ansArr=[];
   const nextButton = () => {
-    
     const nextQuestion = currentQuestion + 1;
+
     if (questions.length - 1 == nextQuestion) {
       setSubmitButton(true);
     }
+
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       alert("are you want to submit");
       setShowScore(true);
     }
+   
   };
+  
   const preButton = () => {
     const preQuestion = currentQuestion - 1;
     if (preQuestion !== -1) {
@@ -77,83 +68,72 @@ export default function Exam() {
     setCurrentQuestion(key - 1);
   };
 
-  const answerButton = (mark) => {
-    if (mark) {
-      setTotalScore(totalScore + 1);
+  const answerButton = (a) => {
+    setAns(a.target.value);
+    if(ans){
+      setIsActive(true);
     }
-  };
-  console.log(totalScore);
-console.log("inputRef",inputRef)
+    ansArr.push(a.target.value);
+    console.log(ansArr);
+    console.log(a.target.value)
+   };
+
+  const onAnswerSubmit=(e)=>{
+    e.preventDefault()
+   
+
+    if(questions[currentQuestion].isCorrect==ans){
+      setTotalScore(totalScore+1)
+     }
+      else{
+        setTotalScore(totalScore)
+     }
+    console.log('total_score',totalScore)
+    
+  }
+
+
   return (
     <>
-      {showScore ? (
-        <Result totalScore={totalScore} questions={questions} />
-      ) : (
-        <>
-          <div className="q-body">
+     <> <div className="q-body">
             {questions.map((item) => {
               return (
-                <button
-                  className={
-                    item.id === currentQuestion + 1
-                      ? " button active"
-                      : "button"
-                  }
-                  key={item.id}
-                  onClick={() => navButton(item.id)}
-                >
-                  {item.id}
-                </button>
+                <button className={ item.id === currentQuestion + 1 ? "btn btn-black active" : "btn btn-black"}
+                  key={item.id} onClick={() => navButton(item.id)}>{item.id}</button>
               );
             })}
           </div>
           <div className="exam-container">
+          <form onSubmit={onAnswerSubmit}>
             <div className="row">
               <div className="questions-wraper">
-                <h1 className="q-heading"> Question : {currentQuestion + 1}</h1>
-                <h5 className="question">
-                  {questions[currentQuestion].questionText}
-                </h5>
+                <h5> Question : {currentQuestion + 1}</h5>
+                <h3 >{questions[currentQuestion].questionText}</h3>
                 <ol className="options">
                   {questions[currentQuestion].answerOptions.map(
                     (item, index) => {
                       return (
                         <div className="radio-label">
-                          <input
-                            type="radio"
-                            name="ans"
-                            id="ans"
-                           className="options-radio"
-                           ref={inputRef} 
-                            key={index}
-                            onChange={() => answerButton(item.isCorrect)}
-                          />
-                          <label className="options-label" htmlFor="ans">
-                            {item.answerText}
-                          </label>
-                        </div>
+                       <input  className={isActive? 'active-btn':'options-radio btn'} key={index}
+                          onClick={(a)=>answerButton(a)} 
+                         value={item.answerText} />
+                         </div>
                       );
                     }
                   )}
-                </ol>
+                </ol> 
               </div>
             </div>
-
-            <button className="button btn-1" onClick={preButton}>
-              Previous Question
-            </button>
-            {submitButton ? (
-              <button className="button btn-2 btn-3" onClick={nextButton}>
-                Submit Exam
-              </button>
-            ) : (
-              <button className="button btn-2" onClick={nextButton}>
-                Next Question
-              </button>
-            )}
-          </div>
+            <div className="d-flex justify-content-between">
+             <button className="btn btn-green" onClick={preButton}> Previous Question </button>
+              {submitButton ?(<button className="btn btn-blue" type="submit" onClick={nextButton} onSubmit={onAnswerSubmit}>Submit Exam</button>)
+              : (<button className="btn btn-black" onClick={nextButton}>Next Question </button>) }
+           </div>
+         </form>
+        </div>
+        
         </>
-      )}
+  
     </>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,66 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React,{useState} from "react";
+import { useHistory } from 'react-router-dom';
 export default function Home() {
+  const [username,setUsername]= useState('')
+  const [password, setPassword] = useState('')
+  const [feedbackText, setFeedbackText] =useState('');
+  let history = useHistory();
+
+  const handleUsername=(e)=>{
+    setUsername(e.target.value)
+  }
+
+  const handlePassword=(e)=>{
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit=(e)=>{
+   e.preventDefault();
+   if(username===''){
+    setFeedbackText('Please fill Username');
+    return;
+   }
+
+   if(password===''){
+    setFeedbackText('Please fill Password');
+    return;
+   }
+   
+   if(username=='admin' && password=='Password'){
+    setFeedbackText('')
+    history.push("/exam");
+   }
+  else{
+    setFeedbackText('Invalid Username or password')
+  }
+   
+  }
+
   return (
     <>
-      <h1 className="heading">Welcome to Exam </h1>
-      <div className="input-container">
-          <h1 className="heading">LOGIN</h1>
-          <input type="text" value="sachin" name="Sachin"/>
-          <input type="text" value="RBT123" name="RBT123"/>
-         <Link to="/exam"> <button className="button">Start Exam</button></Link>
-      </div>
+    <div className='vh-100 d-flex justify-content-between align-items-center'>
+    
+      <form className="input-container d-flex m-auto justify-content-center ">
+        <img alt='logo' src="../Coffee-cup.svg" height={'200px'}/>
+        <h2 className="text-center line-in-text">LOGIN</h2>
+        <div>Enter Your Username</div> 
+        <input className='form-input w-100 ' type="text" name='username ' onChange={handleUsername} value={username}/>
+        <div>Enter Your Password</div>
+        <input className='form-input w-100 ' type="text"  name="password" onChange={handlePassword} value={password}/>
+        <a className="mb-1 text-decoration-underline cursor-pointer">Forgot Username or Password?</a> 
+        <button className="btn  btn-black"  onClick={handleSubmit}> Start Exam</button>
+        <p className='text-red mt-1'>{feedbackText}</p>
+        <div className="line-in-text">or sign in with</div>
+        <button className="btn d-flex justify-content-center align-items-center " disabled={true}>
+         <img src="../google.svg" alt='Google'/>&nbsp;Google
+        </button>
+        <div className="text-center cursor-pointer">Need help? <a className="text-decoration-underline">Visits our help center</a> </div>
+      </form>
+
+    
+      <img alt='login_boy' src="../login_boy.svg"  />
+  
+  </div>
     </>
   );
 }
